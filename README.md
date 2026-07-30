bcc7a9eb298f4ae62f23a60fe5da5584eb478f3d
Neivydis <nelson.carrasco@wizeline.com>
2026-07-30 05:25:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+npm install
+npm run dev
+Open http://localhost:4321
---
+++ astro.config.mjs
@@ -0,0 +1,2 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({ site: 'https://l2guardian.pages.dev' });
